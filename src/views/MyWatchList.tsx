5b2027d0c59ea5e5979fea5df6a7e0055e683879
dontry <mccoy018@gmail.com>
2019-08-16 04:38:27 +1000

    fix login

--- src/views/MyWatchList.tsx
+++ src/views/MyWatchList.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "layout/Container";
+import { connect } from 'react-redux'
 import WatchList from "components/WatchList";
+import { RootState } from "reducers/state";
+import { Dispatch } from "redux";
+import { FETCH_WATCHLIST_REQUEST } from "actions/watchlist";
+import { Show } from "models/Show";
 
-export const MyWatchlist = () => {
-  return (
-    <Container>
-      <WatchList />
-    </Container>
-  );
-};
+interface Props {
+  watchlist: Show[];
+  getWatchlist(): void;
+}
+
+class MyWatchList extends Component<Props> {
+  componentDidMount() {
+    if (this.props.watchlist.length === 0) {
+      this.props.getWatchlist();
+    }
+  }
+  render() {
+    return (
+      <Container>
+        <WatchList />
+      </Container>
+    );
+  }
+}
+
+
+
+const mapStateToProps = (state: RootState) => ({
+  watchlist: state.watchlist,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getWatchlist() { dispatch({ type: FETCH_WATCHLIST_REQUEST }) }
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyWatchList)
 
